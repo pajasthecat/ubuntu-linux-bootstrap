4f71c70135c3822a1612d7da1b650990f3a07a9d
Henrik Forsberg <henrikforsberg@pop-os.localdomain>
2020-06-13 13:13:47 +0200

    Updated docker

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,11 +27,24 @@ dpkg --install google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
 
 # Docker setup - https://docs.docker.com/install/linux/docker-ce/ubuntu/
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update -y
-apt-get install -y docker-ce docker-ce-cli containerd.io
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+   apt-get update
+   apt-get install docker-ce docker-ce-cli containerd.io
 
 # ASP.net setup - https://dotnet.microsoft.com/download/linux-package-manager/ubuntu18-04/sdk-3.0.100
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
